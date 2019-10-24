ddc4ed2ac7e0bc01932fd28f27770277965e13d9
D. Scott Bargabus <scott@bargabus.com>
2019-10-23 22:02:18 -0700

    Added defaults to model, initializing them differently in the fixture

--- idastatus/stations/models.py
+++ idastatus/stations/models.py
@@ -30,52 +30,50 @@ class Station(models.Model):
         ('THREE', 'Third One'),
     )
 
-    addr1 = models.CharField(max_length=50, blank=True,
+    addr1 = models.CharField(default=None, max_length=50, blank=True,
         null=True, help_text='Address field 1')
-    addr2 = models.CharField(max_length=50, blank=True,
+    addr2 = models.CharField(default=None, max_length=50, blank=True,
         null=True, help_text='Address field 2')
-    altcode = models.CharField(max_length=4, blank=True, null=True)
-    city = models.CharField(max_length=50, blank=True,
+    altcode = models.CharField(default=None, max_length=4, blank=True, null=True)
+    city = models.CharField(default=None, max_length=50, blank=True,
         null=True, help_text='City')
-    code = models.CharField(max_length=4, blank=True,
-        null=True, help_text='Station code')
-    country = models.CharField(
+    code = models.CharField(max_length=4, help_text='Station code')
+    country = models.CharField(default=None, 
         max_length=50, blank=True, null=True, help_text='Country')
-    country_code = models.CharField(
-        max_length=2, blank=True, null=True, help_text='Station\'s country code')
-    description = models.CharField(
-        max_length=200, blank=True, null=True, help_text='Station description')
-    elev = models.FloatField(blank=True, null=True, help_text='Elevation')
-    end_t = models.DecimalField(
+    country_code = models.CharField(default=None, max_length=2, help_text='Station\'s country code')
+    description = models.CharField(default=None, 
+        max_length=200, help_text='Station description')
+    elev = models.FloatField(default=None, blank=False, null=True, help_text='Elevation')
+    end_t = models.DecimalField(default=None, 
         max_digits=17, decimal_places=5, blank=True, null=True, help_text='')
     # TODO to be added at a later date
     geo_datum = models.CharField(
         choices=GEODETIC_DATUM, max_length=50, blank=True, null=True, help_text='Geodetic Positioning System')
-    geology_descr = models.CharField(
+    geology_descr = models.CharField(default=None, 
         max_length=15, blank=True, null=True, help_text='')
-    histcode = models.CharField(
+    histcode = models.CharField(default=None, 
         max_length=15, blank=True, null=True, help_text='')
-    lat = models.FloatField(blank=True, null=True, help_text='Latitude')
-    loc_descr = models.CharField(
+    lat = models.FloatField(default=None, blank=False, null=True, help_text='Latitude')
+    loc_descr = models.CharField(default=None, 
         max_length=200, blank=True, null=True, help_text='')
-    lon = models.FloatField(blank=True, null=True, help_text='Longitude')
-    name = models.CharField(max_length=200, blank=True,
+    lon = models.FloatField(default=None, blank=False, null=True, help_text='Longitude')
+    name = models.CharField(default=None, max_length=200, blank=True,
         null=True, help_text='')
-    network_id = models.ForeignKey(
-        Network, null=True, on_delete=models.CASCADE, help_text='')
-    postal_code = models.CharField(
+    network_id = models.ForeignKey(Network, 
+        null=True, on_delete=models.CASCADE, help_text='')
+    postal_code = models.CharField(default=None, 
         max_length=15, blank=True, null=True, help_text='Postal Code')
-    province = models.CharField(
+    province = models.CharField(default=None, 
         max_length=50, blank=True, null=True, help_text='Province')
-    start_t = models.DecimalField(
+    start_t = models.DecimalField(default=None, 
         max_digits=17, decimal_places=5, blank=True, null=True, help_text='')
-    status_code = models.CharField(
+    status_code = models.CharField(default=None, 
         max_length=100, blank=True, null=True, help_text='')
-    status_descr = models.CharField(
+    status_descr = models.CharField(default=None, 
         max_length=100, blank=True, null=True, help_text='')
-    status_updated = models.CharField(
+    status_updated = models.CharField(default=None, 
         max_length=100, blank=True, null=True, help_text='')
-    vault_descr = models.CharField(
+    vault_descr = models.CharField(default=None, 
         max_length=200, blank=True, null=True, help_text='')
 
     def get_absolute_url(self):
@@ -98,7 +96,7 @@ class ChannelEpoch(models.Model):
 
     altcode = models.CharField(max_length=4, blank=True, null=True)
     chan = models.CharField(max_length=4, blank=True, null=True, help_text='')
-    edepth = models.FloatField(blank=True, null=True, help_text='')
+    edepth = models.FloatField(blank=False, null=True, help_text='')
     end_t = models.DateTimeField(blank=True, null=True, help_text='')
     flags = models.CharField(
         max_length=200, blank=True, null=True, help_text='')
