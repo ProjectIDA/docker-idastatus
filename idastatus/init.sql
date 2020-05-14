395e549252e02ea4c8c5d766ddc5099a5e92a977
D. Scott Bargabus <scott@bargabus.com>
2020-05-14 12:43:05 -0700

    Updated postgresql password

--- idastatus/init.sql
+++ idastatus/init.sql
@@ -1,6 +1,6 @@
-create user postgres with encrypted password 'password';
+create user postgres with encrypted password 'fQuDTCP8Js1W';
 grant all privileges on database postgres to postgres;
 
 CREATE DATABASE idastatus;
-create user idadb with encrypted password 'password';
+create user idadb with encrypted password 'fQuDTCP8Js1W';
 grant all privileges on database idastatus to idadb;
