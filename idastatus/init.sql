ef62eff2eb21d5e3fc34e89c59690c3f6aded676
D. Scott Bargabus <scott@bargabus.com>
2020-05-14 16:26:56 -0700

    Changed the password in the Django files to a string that will be replaced with a prompted password by running passwd_update.sh

--- idastatus/init.sql
+++ idastatus/init.sql
@@ -1,6 +1,6 @@
-create user postgres with encrypted password 'fQuDTCP8Js1W';
+create user postgres with encrypted password 'CHANGE_THIS_BEFORE_RUNNING';
 grant all privileges on database postgres to postgres;
 
 CREATE DATABASE idastatus;
-create user idadb with encrypted password 'fQuDTCP8Js1W';
+create user idadb with encrypted password 'CHANGE_THIS_BEFORE_RUNNING';
 grant all privileges on database idastatus to idadb;
