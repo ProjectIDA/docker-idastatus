42b4dfc153c26bb55c58be9f0e1de6acd7b3ae63
D. Scott Bargabus <sbargabus@ucsd.edu>
2022-12-05 19:34:05 -0800

    moved commands back to docker compose yml from ud-local.sh

--- ud-local.sh
+++ ud-local.sh
@@ -3,11 +3,5 @@
 export PATH=/usr/local/bin:$PATH
 
 cd /usr/local/ida/docker-idastatus
-/bin/make build
 /bin/make run
-
-python manage.py makemigrations
-python manage.py migrate 
 /bin/make loadfixtures
-
-gunicorn idastatus.wsgi --bind 0.0.0.0:80  --timeout 3000"    
