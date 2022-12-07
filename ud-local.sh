dd41cf97bee9d2e7cf842b72ebe12de0f69ef471
D. Scott Bargabus <sbargabus@ucsd.edu>
2022-12-06 16:29:34 -0800

    Added in the aws fixture update

--- ud-local.sh
+++ ud-local.sh
@@ -4,4 +4,5 @@ export PATH=/usr/local/bin:$PATH
 
 cd /usr/local/ida/docker-idastatus
 /bin/make run
+aws s3 sync s3://projectida.dcc/fixtures/ ./idastatus/stations/fixtures/
 /bin/make loadfixtures
